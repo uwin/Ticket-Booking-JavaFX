a0c4dd6969c5819afc35546c7e39d9a36e1dbc1d
uwin <Nx9nX?p*jo_e]"7>
2020-04-03 00:28:55 +0530

    don't forget to commit

--- TrainBooking.java
+++ TrainBooking.java
@@ -13,9 +13,9 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import org.bson.Document;
-
 import java.time.LocalDate;
 import java.util.*;
+
 public class TrainBooking extends Application {
     static final int SEATING_CAPACITY = 42;
     static final ArrayList<ArrayList<String>> booking = new ArrayList<>();
@@ -27,22 +27,23 @@ public class TrainBooking extends Application {
     }
     private void welcome() {
         System.out.println("\nwelcome to ticket booking system \n" +
-                "A/C compartment for Denuwara Menike");
+                            "A/C compartment for Denuwara Menike");
         listOption();
     }
 
     /**
      * this method is used to print the menu options & to run the relevant
-     * methods according to user input the code will print a all options of the
-     * menu along with a corresponding letter in front of it.
+     * methods according to user input
+     * the code will print a all options of the menu along with a corresponding
+     * letter in front of it.
      *
-     * the code will request a user input using the user if the methods are
-     * to be run on the console the code will run the relevant methods directly
-     * using a switch case, if it's gui related the firstGui method will be run
-     * by passing the user input to it
+     * the code will request a user input a letter using the given menu the user
+     * if the methods are to be run on the console the code will run the
+     * relevant methods directly using a switch case, if it's gui related the
+     * firstGui method will be run by passing the user input to it
      */
     private void   listOption() {
-        System.out.println("\n\n"+
+        System.out.println("\n"+
                 "A Add a seat\n"+
                 "V View all seats\n"+
                 "E View empty seats\n"+
@@ -53,10 +54,10 @@ public class TrainBooking extends Application {
                 "O List seats\n"+
                 "q quit\n");
         Scanner scanOption= new Scanner(System.in);
-        System.out.println(">> select a option");
+        System.out.println("> select a option");
         String userOption= scanOption.next().toUpperCase();
         switch (userOption) {
-//        calling methods depending on the users input for the previous method
+//        calling methods depending on the users input form the previous method
 //        gui methods will run though first gui
             case "A":
             case "V":
@@ -83,7 +84,7 @@ public class TrainBooking extends Application {
                 break;
             default:
                 System.out.println("invalid input");
-                listOption();
+                waitOption();
                 break;
         }
     }
@@ -94,7 +95,7 @@ public class TrainBooking extends Application {
      *
      * the date, starting stop & ending stop  values will be passed to the gui
      * related methods addOption, viewOption and emptyOption. these methods will
-     * be run using a switch case which uses the user input
+     * be run using a switch case with the use of userOption parameter
      * @param userOption this parameter is used to pass the user inputs for Gui
      *                   related function from the listOptions method
      */
@@ -112,7 +113,7 @@ public class TrainBooking extends Application {
 
 //        main text for first window
         Label head = new Label("Denuwara Menike Ticket Booking System\n" +
-                "                   Colombo-Badulla");
+                "                   A/C compartment");
         head.setFont(new Font("Arial", 30));
         head.setTextFill(Color.web("#0076a3")); //light blue
         first.add(head,20,3,50,8);
@@ -134,8 +135,6 @@ public class TrainBooking extends Application {
         });
         datePick.setValue(LocalDate.now());
         first.add(datePick, 12, 12,18,4);
-
-
 
         String[] stops = new String[]{ "Colombo Fort","Polgahawela",
                 "Peradeniya Junction", "Gampola","Nawalapitiya",
@@ -175,7 +174,7 @@ public class TrainBooking extends Application {
         });
         first.add(switchB,22,20,9,4);
 
-//        gui element to progress to booking page
+//        gui element t o progress to booking page
         Button continueB = new Button("Continue");
         continueB.setMaxSize(120, 60);
         continueB.setOnAction(event -> {
@@ -224,21 +223,23 @@ public class TrainBooking extends Application {
 
 
     /**
-     * this method is used to book seats using grid of 42 icon
-     * checkBSeats method is used to find out the booked seats
-     * if the seat is booked seat it will be styled red
-     * if the seat is not booked it will be styled black
-     *
+     * this method is used to book seats using grid of 42 seat icon
+     * checkBSeats method is used to find out the all booked seats for the
+     * selected route
+     * if the seat no is booked seat it will be styled red
+     * if the seat no is not booked it will be styled black
+     * if the seat no is temporary booked it will  be styled green
      * the gui has 3 text fields that are used to get the user name, surname and
      * the user Nic
      *
      * when the user presses icons to book seat they will react depending on the
+     * state of the seat number
      *
-     * if the seat is booked it will not change
-     * if the seat is temporary booked it'll changed to black & removed from the
+     * if the seat no is booked it will not change
+     * if the seat no is temporary booked it'll changed to black & removed from
+     * the temporary booked list
+     * if the seat is not temporary booked it will be styled green and added to
      * temporary booked list
-     * if the seat is not booked it will be styled green and added to temporary
-     * booked list
      *
      * @param temporaryList this array has 7 values
      *                      (date,from,to,name.seat number, Nic, surname)
@@ -249,11 +250,9 @@ public class TrainBooking extends Application {
      *
      *                      the entered user name, surname & Nic values
      *                      will be assigned to this array to the relevant index
-     *
-     * @param temporarySeat this is array list which contains  seat
-     *                      numbers, which is used to style selected seats and
-     *                      is used to add user data to the main data structure
-     *                      (booking list)
+     * @param temporarySeat this is array list which contains seat numbers,which
+     *                      is used to style selected seats and is used to add
+     *                      user data to the main data structure (booking list)
      * @param stopsList     this array contains a list of all stops for this
      *                      method the values are used to reference when
      *                      checking for booked seats
@@ -282,9 +281,11 @@ public class TrainBooking extends Application {
                 ("images/black.png"));
 //        used to set the seat number
         int number = 1;
+
 //        creating a array of booked seats temporarily for styling
 //        for bookings with same date,from,to as temporary booking
         ArrayList <String>bookedSeat = checkBSeats(temporaryList, stopsList);
+
 //        loop to create seat buttons & seat numbers
         for (int r = 2; r < 5; r++) {
             for (int c = 2; c < 16; c++) {
@@ -307,7 +308,6 @@ public class TrainBooking extends Application {
                     {
                         button.setImage(seatGreen);
                     }
-
                     button.setOnMouseClicked(event ->
                     {
 //        selected seat is already booked change to red
@@ -392,6 +392,7 @@ public class TrainBooking extends Application {
                 a.show();
                 a.setOnCloseRequest(event1 ->
                 {
+//        setting the text fields with the entered values for validation
                     temporaryList.set(3,username.getText());
                     temporaryList.set(5,userId.getText());
                     temporaryList.set(6,surname.getText());
@@ -606,25 +607,28 @@ public class TrainBooking extends Application {
     /**
      * this method is used to delete sets of user data from the main data
      * structure
-     * for this user Nic and date will be taken n as input & the code will
-     * delete all records with the given Nic on the given date
+     * for this given user Nic and date will be taken as input & the code will
+     * delete all records with the given Nic on the given date & will print a
+     * record of the deleted data
      */
     private void deleteOption() {
+//        getting user date in the format yyyy-mm-dd
         Scanner scanDDate = new Scanner(System.in);
         System.out.println("enter Date: [yyyy-mm-dd]");
         String deleteDate= scanDDate.next().toLowerCase();
-        if(deleteDate.equalsIgnoreCase("q")) waitOption();
         ArrayList <String>dateList = new ArrayList<>();
+//        creating a list of all existing dates for verification
         for (ArrayList<String> data : booking) dateList.add(data.get(0));
 
-
+//        getting user Nic
         Scanner scanDNic = new Scanner(System.in);
         System.out.println("enter your Nic: ");
         String deleteNic= scanDNic.next().toLowerCase();
-        if(deleteNic.equalsIgnoreCase("q")) waitOption();
         ArrayList <String>nicList = new ArrayList<>();
+//        creating a list of all existing nic for verification
         for (ArrayList<String> data : booking) nicList.add(data.get(5));
 
+//        array to collect index of user on the main data structure (booking)
         ArrayList <Integer>deleteIndex = new ArrayList<>();
         if (nicList.contains(deleteNic) && dateList.contains(deleteDate))
         {
@@ -645,6 +649,11 @@ public class TrainBooking extends Application {
                         "\nSeat: " + booking.get(i).get(4));
                 booking.remove(i);
             }
+            waitOption();
+        }
+        else if(deleteNic.equals("q")||deleteDate.equals("q"))
+        {
+            waitOption();
         }
         else if(!nicList.contains(deleteNic))
         {
@@ -656,7 +665,6 @@ public class TrainBooking extends Application {
             System.out.println("Date is not in records");
             deleteOption();
         }
-        waitOption();
     }
 
 
@@ -667,6 +675,7 @@ public class TrainBooking extends Application {
      * data related to the given Nic
      */
     private void   findOption() {
+//        getting user Nic
         Scanner scanFNic = new Scanner(System.in);
         System.out.println("enter your Nic: ");
         String findNic= scanFNic.next().toLowerCase();
@@ -854,10 +863,11 @@ public class TrainBooking extends Application {
 
     /**
      * this method is used to get a temporary list for booked seats relevant to
-     * the selected route, this method will check for booked seats between the
-     * starting stop & ending stop, then it'll will check for booked seats
-     * between stops that include the selected route
-     *
+     * the selected route, this method will check for booked seats
+     * > between the starting stop & ending stop
+     * > between routes that include the selected route
+     * > routes that start before the start stop and end before the end stop
+     * > routes that start after the start stop and end after the end stop
      * @param temporaryList this array has 7 values & for this method just the
      *                      starting stop (1st index) and ending stop(2nd index)
      *                      and date(index 0) are used
@@ -872,7 +882,9 @@ public class TrainBooking extends Application {
 //        creating a array for already booked seats
         ArrayList<String> bookedSeat = new ArrayList<>();
 
-//        changing the order of stops in stopList according to selected path
+//        changing the order of stops array according to selected stops
+//        this is done to have colombo to badulla & badulla to colombo
+//        bookings separate
         if (stopsList.indexOf(temporaryList.get(2))
                 <stopsList.indexOf(temporaryList.get(1)))
         {
@@ -909,12 +921,13 @@ public class TrainBooking extends Application {
             startingStop++;
         }
 
-//        getting index of stops immediately & before after booked 2 stops
 
+//        getting index of stop immediately & before start stop
         int afterStop=  stopsList.indexOf(temporaryList.get(2))-1;
         int firstStop=0;
         int lastStop=12;
-//        looping to check for booked seats for routes containing the 2 stops
+//        looping to check for booked seats for routes containing the 2 stops &
+//        for routes that begin before the start stop and end before end stop
         while (firstStop<afterStop)
         {
             int checkStop = afterStop+1;
@@ -945,7 +958,8 @@ public class TrainBooking extends Application {
         int beforeStop= stopsList.indexOf(temporaryList.get(1))+1;
         afterStop=  stopsList.indexOf(temporaryList.get(2));
         firstStop=0;
-//        looping to check for booked seats in between the 2 stops
+//        looping to check for booked seats in between routes that start after
+//        the start stop and end after the end stop
         while (firstStop<beforeStop)
         {
             int checkStop = beforeStop+1;
@@ -953,7 +967,7 @@ public class TrainBooking extends Application {
             {
                 String temporaryStart= stopsList.get(firstStop);
                 String temporaryEnd= stopsList.get(checkStop);
-                System.out.println("ss- "+temporaryStart+" ee- "+temporaryEnd);
+
 
 //        getting  seat numbers using the main data structure
                 for (ArrayList<String> strings : booking)
